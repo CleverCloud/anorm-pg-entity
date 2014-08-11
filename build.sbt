48db030c8c9fb0141c402b91e6fa8f46610ccb0f
Clement Delafargue <clement@delafargue.name>
2014-08-11 17:18:47 +0200

    Tests for generated SQL

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ version := "1.0-SNAPSHOT"
 scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.play" %% "anorm" % "2.3.2"
+  "com.typesafe.play" %% "anorm" % "2.3.2",
+  "org.specs2" %% "specs2" % "2.4" % "test"
 )
 
 scalacOptions ++= Seq(
@@ -13,3 +14,5 @@ scalacOptions ++= Seq(
   "-Ywarn-nullary-override", "-Ywarn-nullary-unit", "-Ywarn-numeric-widen",
   "-Ywarn-value-discard", "-language:postfixOps"
 )
+
+scalacOptions in Test ++= Seq("-Yrangepos")
---
+++ src/test/scala/pgentities/PgEntities.scala
@@ -0,0 +1,61 @@
+import org.specs2._
+
+import java.util.UUID
+
+import pgentity.pg_entity._
+import anorm._
+import anorm.SqlParser._
+import anorm.SqlStatementParser.parse
+
+object Values {
+  case class DummyTable(
+    id: UUID,
+    name: String,
+    number: Int
+  )
+
+  implicit val DummyTablePgEntity = new PgEntity[DummyTable] {
+    val tableName = "dummy-table"
+    val columns = List(PgField("dummy_table_id", Some("UUID")), PgField("name"), PgField("number"))
+    def parser(prefix: String) = {
+      get[UUID](prefix + "dummy_table_id") ~
+      str(prefix + "name") ~
+      int(prefix + "number") map { case (id ~ name ~ number) => DummyTable(id, name, number) }
+    }
+  }
+}
+
+class PgEntitySpec extends mutable.Specification with ScalaCheck {
+  "columnList" should {
+    "automatically prefix fields with the table name" in {
+      val columns = columnList[Values.DummyTable](None).split(",").toList.map(_.trim)
+
+      columns.forall({ c =>
+        c.startsWith("dummy-table.")
+      }) must_== true
+    }
+
+    "prefix fields with the given prefix" in {
+      val prefix = "renamed_column_"
+      val columns = columnList[Values.DummyTable](Some(prefix)).split(",").toList.map(_.trim)
+
+      columns.forall({ c =>
+        c.startsWith("renamed_column_")
+      }) must_== true
+    }
+
+    "include all fields in insert statement" in {
+      val statement = insertSQL[Values.DummyTable]
+      val (_, parsedPlaceholders) = parse(statement)
+
+      parsedPlaceholders must containTheSameElementsAs(Values.DummyTablePgEntity.columns.map(_.name))
+    }
+
+    "include all fields in update statement" in {
+      val statement = updateSQL[Values.DummyTable]()
+      val (_, parsedPlaceholders) = parse(statement)
+
+      parsedPlaceholders must containTheSameElementsAs(Values.DummyTablePgEntity.columns.map(_.name))
+    }
+  }
+}
